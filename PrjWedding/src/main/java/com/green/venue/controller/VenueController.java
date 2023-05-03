91574790348d8c3441ff4eb997085bca3116e922
GGG <GGG@DESKTOP-8N3GG4O>
2023-05-03 16:00:47 +0900

    20230503 서브화면구현

--- PrjWedding/src/main/java/com/green/venue/controller/VenueController.java
+++ PrjWedding/src/main/java/com/green/venue/controller/VenueController.java
@@ -5,10 +5,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VenueController {
-
-	@RequestMapping("/")
-	public String home() {
-		return "home";
+	
+	
+	@RequestMapping("/Venue01")
+	public String venue01() {
+		return "venue/greenhall";
 	}
 	
+	@RequestMapping("/Venue02")
+	public String venue02() {
+		return "venue/redhall";
+	}
+	
+	@RequestMapping("/Venue03")
+	public String venue03() {
+		return "venue/bluehall";
+	}
 }
